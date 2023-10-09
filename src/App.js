1d0af5d18ed2c3b6feaba01e80d6703556651974
Felistus Mbesa <felistusmbesa22@gmail.com>
2023-10-09 09:01:44 +0300

    day 1's work, worked on dark and light theme toggling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Brightness6Icon from '@mui/icons-material/Brightness6';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+//setting the icon when you click on the toggling button
+  const getIcon = () => {
+    if (theme === "light") {
+      return (
+        <>
+          <DarkModeIcon />
+          Dark
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Brightness6Icon />
+          Light
+        </>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-2 bg-primary-light dark:bg-primary-dark">
+      <div className="p-0 flex justify-end items-end">
+        
+        <button className={`text-${theme === 'dark' ? 'text-white' : 'text-black'} cursor-pointer p-6 border border-[var(--primary-light)] dark:border-[var(--primary-dark)] bg-amber-700 dark:bg-primary-dark`} onClick={handleThemeSwitch} >{getIcon()}</button>
+
+      </div>
+      
     </div>
   );
 }
