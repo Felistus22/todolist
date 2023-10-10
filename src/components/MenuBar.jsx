8b77433f45afa65bea6e13d15b3058ae43cd0322
Felistus Mbesa <felistusmbesa22@gmail.com>
2023-10-10 21:56:12 +0300

    finally done with the menu part

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 import {Link} from 'react-router-dom'
 //import Upcoming from './Upcoming'
 import Today from './Today'
 
 function MenuBar() {
+
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav);
+    } 
+
   return (
-    <div className='Menu dark:text-primary-light grid grid-flow-col grid-template-col h-full'>
-        <div className='p-1'>
+    <div className='Menu dark:text-primary-light grid grid-flow-col grid-template-coll sm:grid-template-col h-full'>
+        <div className='p-1 hidden sm:block justify-center items-center'>
             <h1 className='m-1 p-1 text-[var(--primary-text)] dark:text-[var(--secondary-text)] text-xl'>MENU</h1>
             <div className='p-1 m-1'>
-                <input type="search" name="" placeholder='Search' />
+                <input className='sm:w-32 text-primary-dark' type="search" name="" placeholder='Search' />
             </div>
             
             <div className='p-2 m-1 flex flex-col justify-center items-start'>
@@ -18,7 +26,7 @@ function MenuBar() {
                 <span className='cursor-pointer'><Link to='/today'>Today</Link></span>
                 <span>Completed</span>
                 <span>Calender</span>
-                <span>Sticky Wall</span>
+                <span>Sticky Waill</span>
             </div>
 
             <div className='p-2 m-1 flex flex-col justify-center items-start'>
@@ -35,6 +43,27 @@ function MenuBar() {
                 <button type="button">Settings</button>
             </div>
         </div>
+
+        {/*humburgher icon */}
+      <div onClick={handleNav} className='sm:hidden z-10 mt-56 text-primary-text'>
+        <MenuIcon fontSize='large' className='cursor-pointer'/>
+      </div>
+        {/*mobile menu */}
+        <div className={nav ? 'overflow-y-hidden sm:hidden ease-in duration-300 absolute left-0 top-0 h-screen w-[80%] m-12 bg-black/90 px-4 py-7 flex flex-col justify-center items-center' : 'absolute hidden top-0 w-full h-screen left-{-100%} ease-in duration-500'}>
+            <h1 className='m-1 p-1 text-[var(--primary-text)] dark:text-[var(--secondary-text)] text-3xl'>MENU</h1>
+            
+            <div className='p-2 m-1 flex flex-col justify-center items-start text-lg leading-10'>
+                {/*<p className='font-bold text-2xl' >Tasks</p>*/}
+                <span className='cursor-pointer'><Link to='/upcoming'>Upcoming</Link></span>
+                <span className='cursor-pointer'><Link to='/today'>Today</Link></span>
+                <span>Completed</span>
+                <span>Calender</span>
+                <span>Sticky Wall</span>
+                <button type="button">Settings</button>
+            </div>
+
+        </div>
+
 
         
 
--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -4,8 +4,8 @@ function Today() {
   return (
     <div>
       <div className='dark:bg-primary-dark1 bg-secondary-text p-4 grid-flow-row rounded-xl'>
-            <h1 className='p-2 text-2xl font-bold'>Tasks</h1>
-            <div className='grid grid-flow-col gap-4 '>
+            <h1 className='p-2 text-2xl font-bold text-center dark:text-primary-text'>Tasks</h1>
+            <div className='grid sm:grid-flow-col grid-flow-row justify-center gap-4 '>
                 <div className='flex flex-col justify-center'>
                     <h1 className='p-1 text-lg'>Today</h1>
                     <span>go to church</span>
