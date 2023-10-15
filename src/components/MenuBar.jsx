272b0365fe4b4530729a1e628a347fa3ba45a624
Felistus Mbesa <felistusmbesa22@gmail.com>
2023-10-15 16:23:08 +0300

    day6 the workingarea is good, renders component as per menu

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,39 +1,43 @@
 import React, { useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
-import {Link} from 'react-router-dom'
-//import Upcoming from './Upcoming'
+import CloseIcon from '@mui/icons-material/Close';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import UpcomingIcon from '@mui/icons-material/Upcoming';
+import TodayIcon from '@mui/icons-material/Today';
+import FactCheckIcon from '@mui/icons-material/FactCheck';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
+//import {Link} from 'react-router-dom'
+import Upcoming from './Upcoming'
 import Today from './Today'
 
-function MenuBar() {
+function MenuBar({ onSelectComponent }) {
     const [nav, setNav] = useState(false);
-    const [today, setToday] = useState(false);
+    const [isX, setIsX] = useState(false);
+    {/*const [today, setToday] = useState(false);*/}
 
     const handleNav = () => {
         setNav(!nav);
+        setIsX(!isX);
     } 
 
-    const handleToday = () => {
-        setToday(true)
+    const handleComponentSelect = (component) => {
+        onSelectComponent(component);
     }
 
   return (
     <div className='Menu dark:text-primary-light grid grid-flow-col grid-template-coll sm:grid-template-col h-[97vh] bg-primary-light dark:bg-primary-dark'>
         <div className='p-1 hidden sm:block justify-center items-center'>
             <h1 className='m-1 p-1 text-[var(--primary-text)] dark:text-[var(--secondary-text)] text-xl'>MENU</h1>
-            <div className='p-1 m-1'>
-                <input className='sm:w-32 text-primary-dark' type="search" name="" placeholder='Search' />
-            </div>
             
-            <div className='p-2 m-1 flex flex-col justify-center items-start'>
-                <p className='font-bold text-xl' >Tasks</p>
-                <span className='cursor-pointer'><Link to='/upcoming'>Upcoming</Link></span>
-                <span className='cursor-pointer'><Link to='/today' onClick={handleToday}>Today</Link></span>
-                <span>Completed</span>
-                <span>Calender</span>
-                <span>Sticky Wall</span>
+            <div className='p-2 m-1 flex flex-col leading-10 justify-center items-start'>
+                <button onClick={() => handleComponentSelect(<Upcoming />)}><UpcomingIcon fontSize='large' className='text-primary-text p-1' />Upcoming</button>
+                <button onClick={() => handleComponentSelect(<Today />)}><TodayIcon fontSize='large' className='text-primary-text p-1' />Today</button>
+                <span><FactCheckIcon fontSize='large' className='text-primary-text p-1'/>Completed</span>
+                {/*<span>Calender</span>*/}
+                <span><StickyNote2Icon fontSize='large' className='text-primary-text p-1'/>Sticky Wall</span>
             </div>
 
-            <div className='p-2 m-1 flex flex-col justify-center items-start'>
+            {/*<div className='p-2 m-1 flex flex-col justify-center items-start'>
                 <p className='font-bold text-xl'>Tags</p>
                 <p className='grid grid-cols-2 gap-1 text-center'>
                 <span className='bg-primary-text'>TAG1</span>
@@ -41,16 +45,19 @@ function MenuBar() {
                 <button type="button">Add Tag</button>
                 </p>
                 
-            </div>
+  </div>*/}
 
             <div className='p-1 m-1'>
                 <button type="button">Settings</button>
             </div>
         </div>
 
-        {/*humburgher icon grid grid-flow-col grid-template-coll sm:grid-template-col */}
+        {/*humburgher icon */}
       <div onClick={handleNav} className='sm:hidden z-10 mt-56 text-primary-text'>
-        <MenuIcon fontSize='large' className='cursor-pointer'/>
+        {isX? (
+        <CloseIcon fontSize='large' className='cursor-pointer' />):
+        (
+        <MenuIcon fontSize='large' className='cursor-pointer' />)}
       </div>
         {/*mobile menu */}
         <div className={nav ? 'overflow-y-hidden sm:hidden ease-in duration-300 absolute left-0 top-0 h-[107vh] w-[80%] m-12 bg-black/90 px-4 py-7 flex flex-col justify-center items-center' : 'absolute hidden top-0 w-full h-screen left-{-100%} ease-in duration-500'}>
@@ -58,8 +65,8 @@ function MenuBar() {
             
             <div className='p-2 m-1 flex flex-col justify-center items-start text-lg leading-10'>
                 {/*<p className='font-bold text-2xl' >Tasks</p>*/}
-                <span className='cursor-pointer'><Link to='/upcoming'>Upcoming</Link></span>
-                <span className='cursor-pointer'><Link to='/today'>Today</Link></span>
+                <button onClick={() => handleComponentSelect(<Upcoming />)}>Upcoming</button>
+                <button onClick={() => handleComponentSelect(<Today />)}>Today</button>
                 <span>Completed</span>
                 <span>Calender</span>
                 <span>Sticky Wall</span>
--- src/components/WorkingArea.jsx
+++ src/components/WorkingArea.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuBar from './MenuBar'
 import Today from './Today'
 
 function WorkingArea() {
+
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
   return (
     <div className='flex flex-row justify-evenly'>
       <div className=' '>
-        <MenuBar />
+        <MenuBar onSelectComponent={setSelectedComponent}/>
         
       </div>
       <div className='bg-primary-light dark:bg-primary-dark w-full'>
-        <Today />
+        {selectedComponent? (selectedComponent ) 
+        : (<Today />)}
       </div>
     </div>
   )
